fab3698f3b3a0caf53d149ba8058daeea1e440dc
jessica <papylotomba@yahoo.fr>
2020-02-29 22:32:44 -0500

    define variable input, submit, and event listner

--- questions.js
+++ questions.js
@@ -155,3 +155,69 @@ function allDone() {
     createLabel.textContent = "Enter your initials: ";
 
     questionsDiv.appendChild(createLabel);
+
+    // found local storage 
+var allScores = localStorage.getItem("allScores");
+allScores = JSON.parse(allScores);
+
+if (allScores !== null) {
+
+    for (var i = 0; i < allScores.length; i++) {
+
+        var createLi = document.createElement("li");
+        createLi.textContent = allScores[i].initials + " " + allScores[i].score;
+        highScore.appendChild(createLi);
+
+        
+    }
+}
+// Event listener to move to index page
+goBack.addEventListener("click", function () {
+    window.location.replace("./index.html");
+});
+
+// input
+var createInput = document.createElement("input");
+createInput.setAttribute("type", "text");
+createInput.setAttribute("id", "initials");
+createInput.textContent = "";
+
+questionsDiv.appendChild(createInput);
+
+// submit
+var createSubmit = document.createElement("button");
+createSubmit.setAttribute("type", "submit");
+createSubmit.setAttribute("id", "Submit");
+createSubmit.textContent = "Submit";
+
+questionsDiv.appendChild(createSubmit);
+
+// Event listener to capture initials and local storage for initials and score
+createSubmit.addEventListener("click", function () {
+    var initials = createInput.value;
+
+    if (initials === null) {
+
+        console.log("No value entered!");
+
+    } else {
+        var finalScore = {
+            initials: initials,
+            score: timeRemaining
+        }
+        console.log(finalScore);
+        var allScores = localStorage.getItem("allScores");
+        if (allScores === null) {
+            allScores = [];
+        } else {
+            allScores = JSON.parse(allScores);
+        }
+        allScores.push(finalScore);
+        var newScore = JSON.stringify(allScores);
+        localStorage.setItem("allScores", newScore);
+        // Travels to final page
+        window.location.replace("./HighScores.html");
+    }
+});
+
+}
