5fd1854c19cddf4d6453995603e388c09843ef2c
jessica <papylotomba@yahoo.fr>
2020-02-29 20:25:38 -0500

    add eventlisterner, retrieve local storage

--- HighScores.js
+++ HighScores.js
@@ -2,3 +2,20 @@
 var highScore = document.querySelector("#highScore");
 var clear = document.querySelector("#clear");
 var goBack = document.querySelector("#goBack");
+
+// Event listener to clear scores 
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
+// found local storage 
+var allScores = localStorage.getItem("allScores");
+allScores = JSON.parse(allScores);
+
+if (allScores !== null) {
+
+    for (var i = 0; i < allScores.length; i++) {
+
+        var createLi = document.createElement("li");
+        createLi.textContent = allScores[i].initials + " " + allScores[i].score;
+        highScore.appendChild(createLi);
